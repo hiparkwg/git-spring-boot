c9be81d95ddde8b2756e47b3c7726ec2b3c3bf9b
hiparkwg <hiparkwg@gmail.com>
2022-01-06 16:27:21 +0900

    생성

---
+++ git_test_boot/src/main/java/kr/jobtc/GitTestBootApplication.java
@@ -0,0 +1,13 @@
+package kr.jobtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestBootApplication.class, args);
+	}
+
+}
---
+++ git_test_boot/src/main/java/kr/jobtc/TestController.java
@@ -0,0 +1,13 @@
+package kr.jobtc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value="/hi")
+	public String hi() {
+		return "controller hi...";
+	}
+}
